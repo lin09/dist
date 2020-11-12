d3f5721ae57eae02790b6fa54e5fa6fe0e55edfa
lin09 <315713565@163.com>
2020-11-13 07:35:52 +0800

    account-frontend

---
+++ account-frontend/precache-manifest.2594676dfed70105ef389460e1918976.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "037289f5a28616058cffaf8366b2a7ad",
+    "url": "https://cdn.jsdelivr.net/gh/lin09/dist/account-frontend/img/logo.037289f5.svg"
+  },
+  {
+    "revision": "e13a5ad0691c501576970485c0290fce",
+    "url": "https://cdn.jsdelivr.net/gh/lin09/dist/account-frontend/index.html"
+  },
+  {
+    "revision": "29f36ef058fcf4141898",
+    "url": "https://cdn.jsdelivr.net/gh/lin09/dist/account-frontend/js/app.43d98b29.js"
+  },
+  {
+    "revision": "ff29f59546aeaa10bdde",
+    "url": "https://cdn.jsdelivr.net/gh/lin09/dist/account-frontend/js/chunk-vendors.e98a94fd.js"
+  },
+  {
+    "revision": "89e5e16837fb232cb9b231c19f32fe51",
+    "url": "https://cdn.jsdelivr.net/gh/lin09/dist/account-frontend/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "https://cdn.jsdelivr.net/gh/lin09/dist/account-frontend/robots.txt"
+  }
+]);
