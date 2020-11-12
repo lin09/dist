71c8f541fde12c04ebb6d71f65eee83187f845b4
root <root@10.0.2.15>
2020-11-13 07:16:28 +0800

    account-frontend

---
+++ account-frontend/precache-manifest.4ef01dab759e31702d3a37810c62e99c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc035ed075585bbfe71",
+    "url": "/css/app.7eb23ea0.css"
+  },
+  {
+    "revision": "037289f5a28616058cffaf8366b2a7ad",
+    "url": "/img/logo.037289f5.svg"
+  },
+  {
+    "revision": "b1ba9f43e4369567ee457d2d81a41860",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fc035ed075585bbfe71",
+    "url": "/js/app.6a53ed40.js"
+  },
+  {
+    "revision": "b8d06abc6a01aaedceac",
+    "url": "/js/chunk-vendors.7b468a9a.js"
+  },
+  {
+    "revision": "89e5e16837fb232cb9b231c19f32fe51",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
