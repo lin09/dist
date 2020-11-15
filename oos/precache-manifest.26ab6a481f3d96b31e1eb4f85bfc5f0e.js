02a6fa0038a82084d335609f0e2bf1163cc6005e
lin09 <315713565@163.com>
2020-11-15 17:54:45 +0800

    oos

---
+++ oos/precache-manifest.26ab6a481f3d96b31e1eb4f85bfc5f0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a1118376ca355da043",
+    "url": "/css/app.dd14915c.css"
+  },
+  {
+    "revision": "037289f5a28616058cffaf8366b2a7ad",
+    "url": "/img/logo.037289f5.svg"
+  },
+  {
+    "revision": "358fb799a5d988dacf6d5f1746194d30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4a1118376ca355da043",
+    "url": "/js/app.19d62ad3.js"
+  },
+  {
+    "revision": "a3ca5423f1d40b3b079b",
+    "url": "/js/chunk-vendors.bf4b6ca8.js"
+  },
+  {
+    "revision": "7749647d1bd70aa168a0ca86cd2dad97",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
