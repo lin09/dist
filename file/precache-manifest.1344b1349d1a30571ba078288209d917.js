160bab1ddab26b3a7d916638f202e03d74e3c10d
lin09 <315713565@163.com>
2020-11-17 09:49:34 +0800

    oos

---
+++ file/precache-manifest.1344b1349d1a30571ba078288209d917.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "652d41ecf559274ff745552e62415954",
+    "url": "https://cdn.jsdelivr.net/gh/lin09/dist/file/img/logo.652d41ec.svg"
+  },
+  {
+    "revision": "2b23d1e8497fbe3ee4d3a1c575d3d858",
+    "url": "https://file.saileidigi.cn/index.html"
+  },
+  {
+    "revision": "6889bad598f3e4643aac",
+    "url": "https://cdn.jsdelivr.net/gh/lin09/dist/file/js/app.efe4e484.js"
+  },
+  {
+    "revision": "75c8436352a8cf758bf8",
+    "url": "https://cdn.jsdelivr.net/gh/lin09/dist/file/js/chunk-vendors.470dbf74.js"
+  },
+  {
+    "revision": "2ccefd9fdf6039c0a40d8627772cd795",
+    "url": "https://file.saileidigi.cn/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "https://file.saileidigi.cn/robots.txt"
+  }
+]);
