39368187a49ec54a363d96e7514ef6453838d341
lin09 <315713565@163.com>
2020-11-17 21:07:54 +0800

    oos

---
+++ oos/precache-manifest.e4cb12de9549cd39acd54be8eb04508a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55dbacbe56f5cea9ce07",
+    "url": "https://cdn.jsdelivr.net/gh/lin09/dist/oos/css/app.6cdf5811.css"
+  },
+  {
+    "revision": "037289f5a28616058cffaf8366b2a7ad",
+    "url": "https://cdn.jsdelivr.net/gh/lin09/dist/oos/img/logo.037289f5.svg"
+  },
+  {
+    "revision": "8c311d800cfb055cf9a2fd803d20825c",
+    "url": "https://oos.saileidigi.cn/index.html"
+  },
+  {
+    "revision": "55dbacbe56f5cea9ce07",
+    "url": "https://cdn.jsdelivr.net/gh/lin09/dist/oos/js/app.3a0f06eb.js"
+  },
+  {
+    "revision": "a3ca5423f1d40b3b079b",
+    "url": "https://cdn.jsdelivr.net/gh/lin09/dist/oos/js/chunk-vendors.bf4b6ca8.js"
+  },
+  {
+    "revision": "c3068ba16c7ec36860cd0a12dbd84d3d",
+    "url": "https://oos.saileidigi.cn/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "https://oos.saileidigi.cn/robots.txt"
+  }
+]);
--- oos/service-worker.js
+++ oos/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://cdn.jsdelivr.net/gh/lin09/dist/oos/precache-manifest.4dd288bbf62619e61df2787b6896c007.js"
+  "https://cdn.jsdelivr.net/gh/lin09/dist/oos/precache-manifest.e4cb12de9549cd39acd54be8eb04508a.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "system-frontend"});
