cc04b7d2f30f6525ee03570e5e751a6ef351f22f
lin09 <315713565@163.com>
2020-12-12 00:06:39 +0800

    dazi

---
+++ dazi/precache-manifest.4076ac2dd2f4bbd773cbd5dd5fed57d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e3a02167cfb987aa99",
+    "url": "https://cdn.jsdelivr.net/gh/lin09/dist/dazi/css/app.21c07c58.css"
+  },
+  {
+    "revision": "99ba0864e0265193875329153642a45a",
+    "url": "https://cdn.jsdelivr.net/gh/lin09/dist/dazi/img/logo.99ba0864.svg"
+  },
+  {
+    "revision": "fabb1a9ac9889eeee39bdee83dde001d",
+    "url": "/dazi/index.html"
+  },
+  {
+    "revision": "f1e3a02167cfb987aa99",
+    "url": "https://cdn.jsdelivr.net/gh/lin09/dist/dazi/js/app.d36899b8.js"
+  },
+  {
+    "revision": "ab555f5e02924d6cb9aa",
+    "url": "https://cdn.jsdelivr.net/gh/lin09/dist/dazi/js/chunk-vendors.72399471.js"
+  },
+  {
+    "revision": "1f49476027fe85c70cd3bcdd4858c0ee",
+    "url": "/dazi/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dazi/robots.txt"
+  }
+]);
--- dazi/service-worker.js
+++ dazi/service-worker.js
@@ -11,11 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://cdn.jsdelivr.net/npm/workbox-cdn@4.3.1/workbox/workbox-sw.min.js",
-  "https://cdn.jsdelivr.net/gh/lin09/dist/dazi/precache-manifest.93cf53ec1e4406d97bea1d8025c096dc.js"
+  "https://cdn.jsdelivr.net/gh/lin09/dist/dazi/precache-manifest.4076ac2dd2f4bbd773cbd5dd5fed57d7.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "dazi"});
