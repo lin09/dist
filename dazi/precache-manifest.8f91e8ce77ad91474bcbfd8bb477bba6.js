66136628a625de53a9a2b92f202edd2445601516
lin09 <315713565@163.com>
2020-12-11 23:33:31 +0800

    dazi

---
+++ dazi/precache-manifest.8f91e8ce77ad91474bcbfd8bb477bba6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e3a02167cfb987aa99",
+    "url": "https://cdn.jsdelivr.net/gh/lin09/dist/dazi/css/app.21c07c58.css"
+  },
+  {
+    "revision": "99ba0864e0265193875329153642a45a",
+    "url": "https://cdn.jsdelivr.net/gh/lin09/dist/dazi/img/logo.99ba0864.svg"
+  },
+  {
+    "revision": "d5734e32669d9fbd4a6882cd35588038",
+    "url": "/dazi/index.html"
+  },
+  {
+    "revision": "f1e3a02167cfb987aa99",
+    "url": "https://cdn.jsdelivr.net/gh/lin09/dist/dazi/js/app.d36899b8.js"
+  },
+  {
+    "revision": "ab555f5e02924d6cb9aa",
+    "url": "https://cdn.jsdelivr.net/gh/lin09/dist/dazi/js/chunk-vendors.72399471.js"
+  },
+  {
+    "revision": "3bfe47e8ba57077f94c8e22f51ca8082",
+    "url": "/dazi/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dazi/robots.txt"
+  }
+]);
---
+++ dazi/service-worker.js
@@ -0,0 +1,35 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+
+
+importScripts(
+  "https://cdn.jsdelivr.net/npm/workbox-cdn@5.1.4/workbox/workbox-sw.min.js",
+  "https://cdn.jsdelivr.net/gh/lin09/dist/dazi/precache-manifest.8f91e8ce77ad91474bcbfd8bb477bba6.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "dazi"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
